5560d386faa4e9687bafe71675615ca6a810ac19
Cranston Mason <cranstonmasondev@gmail.com>
2017-07-30 00:17:27 +0800

    First commit to Git

---
+++ app/src/main/java/com/lollipop/swap/Tab3.java
@@ -0,0 +1,21 @@
+package com.lollipop.swap;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Cranston Mason on 14/07/2017.
+ */
+
+public class Tab3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_3, container, false);
+    }
+}

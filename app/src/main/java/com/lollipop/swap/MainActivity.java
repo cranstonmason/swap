a7f5ada41d118f722424764b88ba7cc622e57561
Cranston Mason <cranstonmasondev@gmail.com>
2017-08-07 22:17:15 +0800

    Added spinner and list of base currencies.

--- app/src/main/java/com/lollipop/swap/MainActivity.java
+++ app/src/main/java/com/lollipop/swap/MainActivity.java
@@ -17,6 +17,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+
 
     /**
      * The {@link ViewPager} that will host the section contents.
--- app/src/main/java/com/lollipop/swap/Tab1.java
+++ app/src/main/java/com/lollipop/swap/Tab1.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by Cranston Mason on 14/07/2017.
@@ -13,9 +16,28 @@ import android.view.ViewGroup;
 
 public class Tab1 extends Fragment {
 
+    /**
+     * Variables initialized globally
+     */
+    public Spinner baseCurr_spin;
+    public Button checkRate;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_1, container, false);
+
+        View v = inflater.inflate(R.layout.tab_1, container, false); //Initialise a new View
+
+        baseCurr_spin = (Spinner) v.findViewById(R.id.baseCurr_spin); //drop down list of base currencies
+        checkRate = (Button) v.findViewById(R.id.checkRate); //check rate button
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.currency, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_selectable_list_item);
+        baseCurr_spin.setAdapter(adapter);
+
+
+
+        return v;
+
     }
 }
